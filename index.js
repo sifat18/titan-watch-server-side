60cd63bf76113d9c6d65eabc0d30a0affbfdc83e
Sifat <ibnsifat@gmail.com>
2021-11-11 05:28:57 +0600

    order posted

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const app = express()
 const cors = require('cors');
 const { MongoClient } = require('mongodb');
+const ObjectID = require('mongodb').ObjectId;
 
 app.use(cors());
 app.use(express.json())
@@ -16,6 +17,7 @@ async function run() {
         const titanDB = client.db('titan')
         const watchCollection = titanDB.collection('watches')
         const reviewCollection = titanDB.collection('reviews')
+        const orderCollection = titanDB.collection('orders')
 
         // get watches
         app.get('/watch', async (req, res) => {
@@ -23,11 +25,27 @@ async function run() {
             const watchDataArray = await watchData.toArray()
             res.json(watchDataArray)
         })
+        // get watch BYid
+        app.get('/watch/:id', async (req, res) => {
+            console.log('connected')
+            const cursor = req.params.id
+            const filter = { _id: ObjectID(cursor) }
+            const watchData = await watchCollection.findOne(filter)
+            res.json(watchData)
+        })
         // get reviews
         app.get('/reviews', async (req, res) => {
             const reviewData = await reviewCollection.find({})
             const reviewDataArray = await reviewData.toArray()
             res.json(reviewDataArray)
+        })
+        // order post
+        app.post('/order', async (req, res) => {
+            // console.log("posted")
+            const query = req.body;
+            const result = await orderCollection.insertOne(query);
+            // console.log(result)
+            res.json(result)
         })
     } finally {
         // client.close()
